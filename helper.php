da2c3233d3de4759fd03095e78892102dee3eceb
ankio <ankio@ankio.net>
2024-11-13 21:01:47 +0800

    :wrench: (config): 存储配置到 `config.php` 文件
    将应用配置存储到 `config.php` 文件中，方便后续的读取和修改。

--- helper.php
+++ helper.php
@@ -84,6 +84,7 @@ function config($key = null, $set = null): mixed
 {
     if ($set !== null && $key !== null) {
         $GLOBALS['__nova_app_config__'][$key] = $set;
+        file_put_contents(ROOT_PATH . '/config.php', "<?php\nreturn " . var_export($GLOBALS['__nova_app_config__'], true) . ";\n");
         return $set;
     }
     if ($key) {
