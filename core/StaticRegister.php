bc876e6ec3f583cd384d7962faa634e8243f4663
ankio <ankio@ankio.net>
2025-03-28 10:59:48 +0800

    :sparkles: (core): 新增StaticRegister抽象类用于静态注册管理
    新增StaticRegister抽象类，提供静态注册功能，确保子类在注册时调用registerInfo方法。通过Context实例管理注册状态，避免重复注册。

---
+++ core/StaticRegister.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace nova\framework\core;
+
+
+use RuntimeException;
+
+abstract class StaticRegister
+{
+    public static function register(): void
+    {
+        $cls = get_called_class();
+        $key = 'static:' . $cls;
+
+        if (!Context::instance()->get($key, false)) {
+            static::registerInfo();  // 调用抽象方法，等待子类实现
+            Context::instance()->set($key, true);
+        }
+    }
+
+    // 子类必须实现的方法
+    public static function registerInfo(): void{
+        throw new RuntimeException("子类必须重写registerInfo方法");
+    }
+}
